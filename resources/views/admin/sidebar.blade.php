f1bda75b22bbbe00c0333397c319839cb7688144
demo <himanshupatanvadiya2003@gmail.com>
2025-12-24 12:12:52 +0530

    add class in sidebar to link 'active'

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -11,23 +11,23 @@
         </div>
         <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
         <ul class="list-unstyled">
-                <li class="active"><a href="{{url('home')}}"> <i class="icon-home"></i>Home </a></li>
-                <li><a href="{{url('tables')}}"> <i class="icon-grid"></i>Tables </a></li>
-                <li><a href="{{url('charts')}}"> <i class="fa fa-bar-chart"></i>Charts </a></li>
-                <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>Food </a>
+                <li class="{{ request()->is('home') ? 'active' : '' }}"><a href="{{url('home')}}"> <i class="icon-home"></i>Home </a></li>
+                <li class="{{ request()->is('tables') ? 'active' : '' }}"><a href="{{url('tables')}}"> <i class="icon-grid"></i>Tables </a></li>
+                <li class="{{ request()->is('charts') ? 'active' : '' }}"><a href="{{url('charts')}}"> <i class="fa fa-bar-chart"></i>Charts </a></li>
+                <li ><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>Food </a>
                   <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
-                    <li><a href="{{url('add_food')}}">Add Food</a></li>
-                    <li><a href="{{url('add_juice')}}">Add Juice</a></li>
-                    <li><a href="{{url('view_food')}}">View Food</a></li>
-                    <li><a href="{{url('view_Juice')}}">View Juice</a></li>
+                    <li class="{{ request()->is('add_food') ? 'active' : '' }}"><a href="{{url('add_food')}}">Add Food</a></li>
+                    <li class="{{ request()->is('add_juice') ? 'active' : '' }}"><a href="{{url('add_juice')}}">Add Juice</a></li>
+                    <li class="{{ request()->is('view_food') ? 'active' : '' }}"><a href="{{url('view_food')}}">View Food</a></li>
+                    <li class="{{ request()->is('view_juice') ? 'active' : '' }}"><a href="{{url('view_Juice')}}">View Juice</a></li>
                   </ul>
                 </li>
 
-                <li>
+                <li class="{{ request()->is('orders') ? 'active' : '' }}">
                   <a href="{{url('orders')}}"> <i class="icon-new-file"></i>Orders </a>
                 </li>
 
-                <li>
+                <li class="{{ request()->is('reservation') ? 'active' : '' }}">
                   <a href="{{url('reservation')}}"> <i class="icon-writing-whiteboard"></i>Reservation </a>
                 </li>
 
